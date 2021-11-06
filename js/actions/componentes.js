153ae11d93758fcb425f44bf9efa999bc42d1519
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-11-06 17:21:18 -0600

    we validate the form and add the alerts

--- js/actions/componentes.js
+++ js/actions/componentes.js
@@ -3,43 +3,86 @@ import { dragAndDrop } from "./dragAndDrop.js";
 ( function (){
     const inputOpcions = document.querySelectorAll( 'input[name="option"]' );
 
-    const encryptButton = document.querySelector( '.button.encrypt' );
+    let encryptButton;
     const optionAlgorithm = document.querySelector( '#option_algorithm' );
-    const textEntry = document.querySelector( '#text_entry' );
+    let optionText;
+    let buttonSave;
 
     document.addEventListener( 'DOMContentLoaded', () => {
         inputOpcions.forEach( input => {
             input.addEventListener( 'change', showEncryptionOption );
         });
-
-        encryptButton.addEventListener( 'click', validateForm );
     });
 
     function showEncryptionOption () {
         const banner         = document.querySelector( '.banner' ); 
-        const optionText     = document.querySelector( '.option__text' );
+        optionText           = document.querySelector( '.option__text' );
         const optionDocument = document.querySelector( '.option__document' );
-
         banner.style.display = 'none';
+
         if( this.id === 'texto' ) {
+            disableButton( optionText );
             optionText.style.display     = 'flex';
             optionDocument.style.display = 'none';
+
+            encryptButton = optionText.querySelector( '.button.encrypt' )
+            encryptButton.addEventListener( 'click', validateFormOptionText );
         } else {
+            disableButton( optionDocument );
             optionText.style.display     = 'none';
             optionDocument.style.display = 'flex';
             dragAndDrop();
+
+            encryptButton = optionDocument.querySelector( '.button.encrypt' )
+            // encryptButton.addEventListener( 'click', validateFormOptionText );
         }
     }
+    
+    function disableButton ( reference ) {
+        buttonSave = reference.querySelector( '.button.save' );
+        buttonSave.disabled = true;
+        buttonSave.classList.add( 'disable' );
+    }
 
-    function validateForm ( e ) {
+    function validateFormOptionText ( e ) {
         e.preventDefault();
+        const textEntry = document.querySelector( '#text_entry' );
 
-        if( e.target.parentNode.parentNode.classList.contains( 'option__text' ) && optionAlgorithm.value !== '' && textEntry.value !== '' ) {
-            console.log( 'optionAlgorithm' + textEntry.value );
-        }
-        if( e.target.parentNode.parentNode.classList.contains( 'option__document' )  ) {
+        if( optionAlgorithm.value === '' ) {
+            optionAlgorithm.classList.add( 'error' );
+            setTimeout( () => optionAlgorithm.classList.remove( 'error' ), 2000 );
+            showMessageError( optionText, 'Todos los campos son obligatorios' );
+            return;
+        } else if( textEntry.value.trim() === '' ) {
+            textEntry.classList.add( 'error' );
+            setTimeout( () => textEntry.classList.remove( 'error' ), 2000 );
+            showMessageError( optionText, 'Todos los campos son obligatorios' );
+            return;
+        } 
+
+        textEntry.classList.remove( 'error' );
+        optionAlgorithm.classList.remove( 'error' );
+        encryptText( 'texto' );
+    }
+
+    function showMessageError( reference, menssage ) {
+        const messageIsActive = document.querySelector( '.message__error' );
+
+        if( !messageIsActive ) {
+            const divError = document.createElement( 'div' );
+            divError.classList.add( 'message__error' );
+            divError.innerHTML = menssage;
             
+            reference.insertBefore( divError, reference.querySelector( 'container__text-areas' ) );
+
+            setTimeout(() => {
+                divError.remove();
+            }, 2000)
         }
     }
 
+    function encryptText ( text ) {
+        console.log( text );
+    }
 })();
+
